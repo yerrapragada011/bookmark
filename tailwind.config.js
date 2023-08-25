50dfa1ece4b890f7d840108065a9e5a3a352fe70
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-25 14:54:23 -0400

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,23 @@ module.exports = {
     screens: {
       sm: '480px',
       md: '768px',
-      ld: '1020px',
+      lg: '1020px',
       xl: '1440px'
     },
-    colors: {
-      softBlue: 'hsl(231, 69%, 60%)',
-      softRed: 'hsl(0, 94%, 66%)',
-      grayishBlue: 'hsl(229, 8%, 60%)',
-      veryDarkBlue: 'hsl(229, 31%, 21%)'
-    },
-    fontFamily: {
-      sans: ['Rubik', 'sans-serif']
-    },
-    extend: {}
+    extend: {
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)'
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif']
+      },
+      backgroundImage: () => ({
+        dots: "url('../images/bg-dots.svg')"
+      })
+    }
   },
   plugins: []
 }
